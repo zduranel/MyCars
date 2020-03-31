ff4bcf907af000e58fcb6a0761ccb4fc16acb5de
zduranel <duranel78@gmail.com>
2020-03-31 18:14:04 +0300

    Proje İlk Upload

---
+++ home/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Setting(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    company = models.CharField(max_length=150)
+    adress = models.CharField(blank=True,max_length=150)
+    phone = models.CharField(blank=True,max_length=150)
+    fax = models.CharField(blank=True,max_length=150)
+    email = models.CharField(blank=True,max_length=150)
+    smtpserver = models.CharField(blank=True,max_length=150)
+    smptemail = models.CharField(blank=True,max_length=150)
+    smptpassword = models.CharField(blank=True,max_length=150)
+    smptport = models.CharField(blank=True,max_length=5)
+    icon = models.ImageField(blank=True,upload_to='images/')
+    facebook   = models.CharField(blank=True,max_length=150)
+    instagram = models.CharField(blank=True,max_length=150)
+    twitter = models.CharField(blank=True,max_length=150)
+
+    aboutus = models.TextField(blank=True)
+    contact = models.TextField(blank=True)
+    references = models.TextField(blank=True)
+
+    status = models.CharField(max_length=10,choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
---
+++ product/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+# Register your models here.
+from product.models import Category, Product, Images
+
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','category','price','amount', 'status','image_tag']
+    readonly_fields = ('image_tag',)
+    list_filter = ['category', 'status']
+    inlines = [ProductImageInline]
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product','image_tag']
+    readonly_fields = ('image_tag',)
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Images,ImagesAdmin)
