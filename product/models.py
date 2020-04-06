22312eded5604964bf33e0acf6cb0a64088ba547
zduranel <duranel78@gmail.com>
2020-04-07 02:30:51 +0300

    güncelleme genel

--- product/models.py
+++ product/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.safestring import mark_safe
-
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Category(models.Model):
     STATUS = (
@@ -35,7 +35,7 @@ class Product(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     price = models.FloatField()
     amount=models.IntegerField()
-    detail = models.TextField()
+    detail = RichTextUploadingField()
 
     status = models.CharField(max_length=10,choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
