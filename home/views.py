b1bc929e1e0c0196c1a81f441297b4493bc9c4dc
zduranel <duranel78@gmail.com>
2020-04-12 22:20:59 +0300

    gece içinde güncellenecek bazı hatalar var

--- home/views.py
+++ home/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from home.models import Setting, ContactFormu, ContactFormMessage
-from product.models import Product
+from product.models import Product, Category
 
 
 def index(request):
     setting = Setting.objects.get(pk=1)
     sliderdata = Product.objects.all()[:4]
+    category =  Category.objects.all()
     context = {'setting': setting,
+               'category': category,
                'page':'home',
                'sliderdata' : sliderdata}
     return render(request, 'index.html', context)
