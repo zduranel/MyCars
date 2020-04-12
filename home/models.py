621c5cf20990bc5980c10245611c54093926fefd
zduranel <duranel78@gmail.com>
2020-04-12 16:28:27 +0300

    güncelleme genel

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 # Create your models here.
+from django.forms import TextInput, ModelForm, Textarea
 
 
 class Setting(models.Model):
@@ -14,27 +15,61 @@ class Setting(models.Model):
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     company = models.CharField(max_length=150)
-    adress = models.CharField(blank=True,max_length=150)
-    phone = models.CharField(blank=True,max_length=150)
-    fax = models.CharField(blank=True,max_length=150)
-    email = models.CharField(blank=True,max_length=150)
-    smtpserver = models.CharField(blank=True,max_length=150)
-    smptemail = models.CharField(blank=True,max_length=150)
-    smptpassword = models.CharField(blank=True,max_length=150)
-    smptport = models.CharField(blank=True,max_length=5)
-    icon = models.ImageField(blank=True,upload_to='images/')
-    facebook   = models.CharField(blank=True,max_length=150)
-    instagram = models.CharField(blank=True,max_length=150)
-    twitter = models.CharField(blank=True,max_length=150)
+    adress = models.CharField(blank=True, max_length=150)
+    phone = models.CharField(blank=True, max_length=150)
+    fax = models.CharField(blank=True, max_length=150)
+    email = models.CharField(blank=True, max_length=150)
+    smtpserver = models.CharField(blank=True, max_length=150)
+    smptemail = models.CharField(blank=True, max_length=150)
+    smptpassword = models.CharField(blank=True, max_length=150)
+    smptport = models.CharField(blank=True, max_length=5)
+    icon = models.ImageField(blank=True, upload_to='images/')
+    facebook = models.CharField(blank=True, max_length=150)
+    instagram = models.CharField(blank=True, max_length=150)
+    twitter = models.CharField(blank=True, max_length=150)
 
     aboutus = RichTextUploadingField(blank=True)
     contact = RichTextUploadingField(blank=True)
     references = RichTextUploadingField(blank=True)
 
-    status = models.CharField(max_length=10,choices=STATUS)
+    status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.title
+
+
+class ContactFormMessage(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Read', 'Read'),
+        ('Closed', 'Closed'),
+
+    )
+    name = models.CharField(blank=True, max_length=20)
+    email = models.CharField(blank=True, max_length=50)
+    subject = models.CharField(blank=True, max_length=50)
+    message = models.CharField(blank=True, max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=20)
+    note = models.CharField(blank=True, max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ContactFormu(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name', 'email', 'subject', 'message']
+
+    widgets = {
+        'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Name & Surname'}),
+        'subject': TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}),
+        'email': TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Adress'}),
+        'message': Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message', 'row': '5'}),
+
+    }
--- home/views.py
+++ home/views.py
@@ -1,33 +1,46 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
     setting = Setting.objects.get(pk=1)
-
     context = {'setting': setting,'page':'home'}
     return render(request, 'index.html', context)
 
 
 def hakkimizda(request):
     setting = Setting.objects.get(pk=1)
-
-    context = {'setting': setting,'page':'hakkimizda'}
+    context = {'setting': setting}
     return render(request, 'hakkimizda.html', context)
 
 
 def referanslar(request):
     setting = Setting.objects.get(pk=1)
-
-    context = {'setting': setting,'page':'referanslar'}
+    context = {'setting': setting}
     return render(request, 'referanslar.html', context)
 
 
 def iletisim(request):
-    setting = Setting.objects.get(pk=1)
 
-    context = {'setting': setting,'page':'iletisim'}
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesaj Gönderildi! Teşekkürler")
+            return HttpResponseRedirect ('/iletisim')
+
+
+    setting = Setting.objects.get(pk=1)
+    form= ContactFormu()
+    context = {'setting': setting,'form':'form'}
     return render(request, 'iletisim.html', context)
